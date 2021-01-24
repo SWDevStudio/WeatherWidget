74b66213c547de39ed54f9a520b279fcc61a2cbd
Kotaro <swdevstudio@gmail.com>
2021-01-24 20:53:56 +0300

    added font-awesome

--- src/main.js
+++ src/main.js
@@ -4,11 +4,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import sendRequest from "@/plugins/sendRequest";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(sendRequest)
 Vue.config.productionTip = false
 
 import '@/assets/global.styl'
+
+
+
 
 
 new Vue({
